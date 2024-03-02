ae2fbfaaca5d9dde72305787c2b519962617d0c6
robertnen <robert.nenciu@yahoo.com>
2024-03-02 02:24:00 +0200

    Update squares_of_a_sorted_array.cpp

--- C++/squares_of_a_sorted_array.cpp
+++ C++/squares_of_a_sorted_array.cpp
@@ -1,5 +1,11 @@
 #include <bits/stdc++.h>
 
+/**
+ * Runtime: beats 43.10% of users with C++
+ * Memory: beats 28.90% of users with C++
+ * Daily problem (Streak 18)
+*/
+
 class Solution {
 public:
     std::vector<int> sortedSquares(std::vector<int>& nums) {
