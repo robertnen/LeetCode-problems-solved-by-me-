40564739cf65c3c5e8fbba408589a4616ca03677
robertnen <robert.nenciu@yahoo.com>
2024-04-02 19:52:48 +0300

    Create isomorphic_strings.py

---
+++ Python/isomorphic_strings.py
@@ -0,0 +1,21 @@
+# Runtime: beats 82.33% of users with Python3
+# Memory: beats 64.59% of users with Python3
+# Daily problem (Streak 49)
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        s_t = {}
+        t_s = {}
+
+        for cS, cT in zip(s, t):
+            if cS in s_t:
+                if s_t[cS] != cT:
+                    return False
+            else:
+                if cT in t_s:
+                    return False
+
+                s_t[cS] = cT
+                t_s[cT] = cS
+
+        return True
