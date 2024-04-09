0be551588d8c2d8462ea9c64550f38c76ff503f4
robertnen <robert.nenciu@yahoo.com>
2024-04-09 04:02:45 +0300

    Create time_needed_to_buy_tickets.cpp

---
+++ C++/time_needed_to_buy_tickets.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+/**
+ * Runtime: beats 100% of users with C++
+ * Memory: beats 46.11% of users with C++
+ * Daily problem (Streak 55)
+*/
+
+class Solution {
+public:
+    int timeRequiredToBuy(std::vector<int>& tickets, int k) {
+        int time = 0;
+        char i, n = tickets.size();
+
+        for(i = 0; i < n; i++)
+            if(i <= k) time += std::min(tickets[i], tickets[k]);
+            else time += std::min(tickets[i], tickets[k] - 1);
+
+        return time;
+    }
+};
